b3766abbf9990a076ae75f38296158319b39d387
Mohammad Yousuf Minhaj Zia <e0381439@u.nus.edu>
2020-06-03 20:58:47 +0800

    reverted some changes with interop

--- public/app.js
+++ public/app.js
@@ -16,106 +16,6 @@ let nameId = null;
 let numberOfDisplayedStreams = 1;
 let numberOfConnectedPeers = 0;
 let muteState = false;
-let sdp = null;
-
-function preferCodec(sdp, codecName) {
-    var info = splitLines(sdp);
-
-    if (!info.videoCodecNumbers) {
-        return sdp;
-    }
-
-    if (codecName === 'vp8' && info.vp8LineNumber === info.videoCodecNumbers[0]) {
-        return sdp;
-    }
-
-    if (codecName === 'vp9' && info.vp9LineNumber === info.videoCodecNumbers[0]) {
-        return sdp;
-    }
-
-    if (codecName === 'h264' && info.h264LineNumber === info.videoCodecNumbers[0]) {
-        return sdp;
-    }
-
-    sdp = preferCodecHelper(sdp, codecName, info);
-
-    return sdp;
-}
-
-
-function preferCodecHelper(sdp, codec, info, ignore) {
-    var preferCodecNumber = '';
-
-    if (codec === 'vp8') {
-        if (!info.vp8LineNumber) {
-            return sdp;
-        }
-        preferCodecNumber = info.vp8LineNumber;
-    }
-
-    if (codec === 'vp9') {
-        if (!info.vp9LineNumber) {
-            return sdp;
-        }
-        preferCodecNumber = info.vp9LineNumber;
-    }
-
-    if (codec === 'h264') {
-        if (!info.h264LineNumber) {
-            return sdp;
-        }
-
-        preferCodecNumber = info.h264LineNumber;
-    }
-
-    var newLine = info.videoCodecNumbersOriginal.split('SAVPF')[0] + 'SAVPF ';
-
-    var newOrder = [preferCodecNumber];
-
-    if (ignore) {
-        newOrder = [];
-    }
-
-    info.videoCodecNumbers.forEach(function(codecNumber) {
-        if (codecNumber === preferCodecNumber) return;
-        newOrder.push(codecNumber);
-    });
-
-    newLine += newOrder.join(' ');
-
-    sdp = sdp.replace(info.videoCodecNumbersOriginal, newLine);
-    return sdp;
-}
-
-
-function splitLines(sdp) {
-    var info = {};
-    sdp.split('\n').forEach(function(line) {
-        if (line.indexOf('m=video') === 0) {
-            info.videoCodecNumbers = [];
-            line.split('SAVPF')[1].split(' ').forEach(function(codecNumber) {
-                codecNumber = codecNumber.trim();
-                if (!codecNumber || !codecNumber.length) return;
-                info.videoCodecNumbers.push(codecNumber);
-                info.videoCodecNumbersOriginal = line;
-            });
-        }
-
-        if (line.indexOf('VP8/90000') !== -1 && !info.vp8LineNumber) {
-            info.vp8LineNumber = line.replace('a=rtpmap:', '').split(' ')[0];
-        }
-
-        if (line.indexOf('VP9/90000') !== -1 && !info.vp9LineNumber) {
-            info.vp9LineNumber = line.replace('a=rtpmap:', '').split(' ')[0];
-        }
-
-        if (line.indexOf('H264/90000') !== -1 && !info.h264LineNumber) {
-            info.h264LineNumber = line.replace('a=rtpmap:', '').split(' ')[0];
-        }
-    });
-
-    return info;
-}
 
 function muteToggleEnable() {
     document.querySelector('#muteButton').addEventListener('click', () => {
@@ -242,9 +142,6 @@ function receiveStream(peerConnection, remoteEndpointID) {
 
     document.getElementById("videos").appendChild(peerNode);
 
-    //document.getElementById(remoteEndpointID).removeAttribute("autoplay");
-    //document.getElementById(remoteEndpointID).removeAttribute("muted");
-
     document.getElementById(remoteEndpointID).srcObject = new MediaStream();
 
     peerConnection.addEventListener('track', event => {
@@ -257,8 +154,6 @@ function receiveStream(peerConnection, remoteEndpointID) {
     });
 
     document.querySelector("#" + remoteEndpointID).muted = false;
-
-    //document.getElementById(remoteEndpointID).play();
 
     mutePeerToggleEnable(remoteEndpointID);
 }
@@ -277,9 +172,6 @@ async function sendOffer(offer, roomRef, peerId) {
             sdp: offer.sdp,
         },
     };
-    peerOffer['offer'].sdp = peerOffer['offer'].sdp.concat(" a=orientation");
-    sdp = peerOffer['offer'].sdp;
-    peerOffer['offer'].sdp = preferCodec(peerOffer['offer'].sdp, "h264");
     await roomRef.collection(peerId).doc('SDP').collection('offer').doc(nameId).set(peerOffer);
 }
 
@@ -290,9 +182,6 @@ async function sendAnswer(answer, roomRef, peerId) {
             sdp: answer.sdp,
         },
     };
-    peerAnswer['offer'].sdp = peerOffer['offer'].sdp.concat(" a=orientation");
-    sdp = peerAnswer['answer'].sdp;
-    peerAnswer['answer'].sdp = preferCodec(peerAnswer['answer'].sdp, "h264");
     await roomRef.collection(peerId).doc('SDP').collection('answer').doc(nameId).set(peerAnswer);
 }
 
