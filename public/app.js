fa37f51b5ebd6342b45dcb685e0a9790def3d2be
Mohammad Yousuf Minhaj Zia <e0381439@u.nus.edu>
2020-06-17 22:54:23 +0800

    added base content signalling

--- public/app.js
+++ public/app.js
@@ -12,11 +12,17 @@ const configuration = {
 
 let roomDialog = null;
 let nameId = null;
+let contentId = null;
 let muteState = false;
 let videoState = true;
+let contentState = false;
 let numberOfDisplayedPeers = 1;
+let screenState = false;
+let cameraStream = null;
+let captureStream = null;
+let localStream = null;
 
-function videoToggleEnable () {
+function videoToggleEnable() {
     document.getElementById('videoButton').addEventListener('click', () => {
         if (videoState) {
             videoState = false;
@@ -30,6 +36,89 @@ function videoToggleEnable () {
             document.querySelector('#videoButton').classList.remove('toggle');
         }
     });
+}
+
+
+async function signalContentShare(roomRef) {
+    contentId =  await addUserToRoom(roomRef);
+
+    roomRef.collection(contentId).doc('SDP').collection('offer').onSnapshot(async snapshot => {
+        await snapshot.docChanges().forEach(async change => {
+            if (change.type === 'added') {
+                peerId = change.doc.id;
+                const peerConnection = new RTCPeerConnection(configuration);
+                sendStream(peerConnection);
+                signalICECandidates(peerConnection, roomRef, peerId, contentId)
+                await receiveOffer(peerConnection, roomRef, peerId, contentId);
+                const answer = await createAnswer(peerConnection);
+                await sendAnswer(answer, roomRef, peerId, contentId);
+                await receiveICECandidates(peerConnection, roomRef, peerId, contentId);
+                document.querySelector('#hangupBtn').addEventListener('click', () => peerConnection.close());
+                restartConnection(peerConnection, roomRef, peerId);
+            } else {
+                console.log("Content Sharing has been setup");
+            }
+        })
+    });
+
+    roomRef.onSnapshot(async snapshot => {
+        const peers = snapshot.data().names
+        console.log("Current peers: " + peers);
+        if (peers[peers.length - 1] != contentId && peers[peers.length - 1] != nameId) {
+            console.log('Sending request to: ' + peers[peers.length - 1]);
+            await peerRequestConnection(peers[peers.length - 1], roomRef, contentId);
+        }
+    });
+}
+
+async function contentToggleButton(roomRef, peerConnection) {
+    if (!screenState) {
+        screenState = true;
+        const displayMediaOptions = {
+            video: {
+                cursor: "always"
+            },
+            audio: true
+        };
+        captureStream = await startCapture(displayMediaOptions);
+        localStream = captureStream;
+        document.getElementById('screenShareButton').innerText = "stop_screen_share";
+        document.getElementById('screenShareButton').classList.add('toggle');
+        captureStream.getVideoTracks()[0].onended = async function () {
+            await contentToggleButton(roomRef, peerConnection);
+        };
+        await signalContentShare(roomRef);
+    } else {
+        stopCapture(captureStream); 
+
+        contentId = null;
+
+        localStream = cameraStream;
+        await roomRef.collection('disconnected').doc().set({
+            disconnected: contentId
+        });
+        screenState = false;
+        document.getElementById('screenShareButton').innerText = 'screen_share';
+        document.getElementById('screenShareButton').classList.remove('toggle');
+    }
+}
+
+async function startCapture(displayMediaOptions) {
+    let captureStream = null;
+
+    try {
+        captureStream = await navigator.mediaDevices.getDisplayMedia(displayMediaOptions);
+    } catch(err) {
+        console.error("Error: " + err);
+    }
+    return captureStream;
+}
+
+function stopCapture(stream) {
+    let tracks = stream.getTracks();
+
+    tracks.forEach(track => track.stop());
+    stream = null;
 }
 
 function muteToggleEnable() {
@@ -47,6 +136,22 @@ function muteToggleEnable() {
             document.querySelector('#muteButton').innerText = "mic";
             document.getElementById('muteButton').classList.remove('toggle');
         }
+    });
+}
+
+function switchStream(peerConnection, stream) {
+    document.getElementById('localVideo').srcObject = stream;
+    let videoTrack = stream.getVideoTracks()[0];
+    var sender = peerConnection.getSenders().find(function(s) {
+        return s.track.kind == videoTrack.kind;
+    });
+    console.log('found sender:', sender);
+    sender.replaceTrack(videoTrack);
+}
+
+function addStream(peerConnection, stream) {
+    stream.getTracks().forEach(track => {
+        peerConnection.addTrack(track, stream);
     });
 }
 
@@ -76,7 +181,7 @@ function signalHangup(roomRef) {
     });
 }
 
-function signalICECandidates(peerConnection, roomRef, peerId) {
+function signalICECandidates(peerConnection, roomRef, peerId, nameId) {
     const callerCandidatesCollection = roomRef.collection(nameId);
     peerConnection.addEventListener('icecandidate', event => {
         if (!event.candidate) {
@@ -92,7 +197,7 @@ function signalICECandidates(peerConnection, roomRef, peerId) {
     });
 }
 
-async function receiveICECandidates(peerConnection, roomRef, remoteEndpointID) {
+async function receiveICECandidates(peerConnection, roomRef, remoteEndpointID, nameId) {
     roomRef.collection(remoteEndpointID).where("name", "==", nameId).onSnapshot(snapshot => {
         snapshot.docChanges().forEach(async change => {
             if (change.type === 'added'&& change.doc.id != "SDP") {
@@ -106,23 +211,26 @@ async function receiveICECandidates(peerConnection, roomRef, remoteEndpointID) {
 }
 
 async function addUserToRoom(roomRef) {
+    let Id;
     await roomRef.get().then(async snapshot => {
         if (!snapshot.exists) { 
-            nameId = "peer1";
+            Id = "peer1";
             await roomRef.set({
-                names : [nameId]
+                names : [Id]
             });
         } else {
-            nameId = "peer" + (snapshot.data().names.length + 1);
+            Id = "peer" + (snapshot.data().names.length + 1);
             await roomRef.update({
-                names: firebase.firestore.FieldValue.arrayUnion(nameId)
+                names: firebase.firestore.FieldValue.arrayUnion(Id)
             });
         }
-        console.log("NameId: " + nameId);
+        console.log("NameId: " + Id);
     });
+
+    return Id;
 }
 
-async function receiveAnswer(peerConnection, roomRef, peerId) {
+async function receiveAnswer(peerConnection, roomRef, peerId, nameId) {
     roomRef.collection(nameId).doc('SDP').collection('answer').doc(peerId).onSnapshot(async snapshot => {
         if (snapshot.exists) {
             const data = snapshot.data();
@@ -147,12 +255,12 @@ function receiveStream(peerConnection, remoteEndpointID) {
 
 
 function sendStream(peerConnection) {
-    document.querySelector('#localVideo').srcObject.getTracks().forEach(track => {
+    localStream.getTracks().forEach(track => {
         peerConnection.addTrack(track, document.querySelector('#localVideo').srcObject);
     });
 }
 
-async function sendOffer(offer, roomRef, peerId) {
+async function sendOffer(offer, roomRef, peerId, nameId) {
     const peerOffer = {
         'offer': {
             type: offer.type,
@@ -162,7 +270,7 @@ async function sendOffer(offer, roomRef, peerId) {
     await roomRef.collection(peerId).doc('SDP').collection('offer').doc(nameId).set(peerOffer);
 }
 
-async function sendAnswer(answer, roomRef, peerId) {
+async function sendAnswer(answer, roomRef, peerId, nameId) {
     const peerAnswer = {
         'answer': {
             type: answer.type,
@@ -172,7 +280,7 @@ async function sendAnswer(answer, roomRef, peerId) {
     await roomRef.collection(peerId).doc('SDP').collection('answer').doc(nameId).set(peerAnswer);
 }
 
-async function receiveOffer(peerConnection1, roomRef, peerId) {
+async function receiveOffer(peerConnection1, roomRef, peerId, nameId) {
     await roomRef.collection(nameId).doc('SDP').collection('offer').doc(peerId).get().then(async snapshot => {
         if (snapshot.exists) {
             const data = snapshot.data();
@@ -197,61 +305,74 @@ function closeConnection(peerConnection, roomRef, peerId) {
 
     peerConnection.onconnectionstatechange = function() {
         if (peerConnection.connectionState == "failed") {
-                peerConnection.close();    
-                document.getElementById(peerId + "Container").remove();
-                enforceGridRules(--numberOfDisplayedPeers);
+            peerConnection.close();    
+            document.getElementById(peerId + "Container").remove();
+            enforceGridRules(--numberOfDisplayedPeers);
         }
     }
 }
 
-async function peerRequestConnection(peerId, roomRef) {
+async function peerRequestConnection(peerId, roomRef, nameId) {
     console.log('Create PeerConnection with configuration: ', configuration);
     const peerConnection = new RTCPeerConnection(configuration);
 
     registerPeerConnectionListeners(peerConnection);
     sendStream(peerConnection)
 
-    signalICECandidates(peerConnection, roomRef, peerId);
+    signalICECandidates(peerConnection, roomRef, peerId, nameId);
     const offer = await createOffer(peerConnection);
 
-    await sendOffer(offer, roomRef, peerId);
+    await sendOffer(offer, roomRef, peerId, nameId);
 
     receiveStream(peerConnection, peerId);
 
-    await receiveAnswer(peerConnection, roomRef, peerId); 
+    await receiveAnswer(peerConnection, roomRef, peerId, nameId); 
 
-    await receiveICECandidates(peerConnection, roomRef, peerId);
+    await receiveICECandidates(peerConnection, roomRef, peerId, nameId);
 
     document.querySelector('#hangupBtn').addEventListener('click', () => peerConnection.close());
 
     closeConnection(peerConnection, roomRef, peerId);
-    
+
     restartConnection(peerConnection, roomRef, peerId);
+    
+    document.querySelector('#screenShareButton').addEventListener('click', async () => await contentToggleButton(roomRef, peerConnection));
+    document.getElementById('localVideo').srcObject.getVideoTracks()[0].onended = async () => {
+        await contentToggleButton(roomRef, peerConnection);
+    };
+    document.querySelector('#screenShareButton').classList.remove("hidden");
 }
 
-async function peerAcceptConnection(peerId, roomRef) {
+async function peerAcceptConnection(peerId, roomRef, nameId) {
     console.log('Create PeerConnection with configuration: ', configuration)
     const peerConnection = new RTCPeerConnection(configuration);
     registerPeerConnectionListeners(peerConnection);
     sendStream(peerConnection);
 
-    signalICECandidates(peerConnection, roomRef, peerId)
+    signalICECandidates(peerConnection, roomRef, peerId, nameId)
 
     receiveStream(peerConnection, peerId);
 
-    await receiveOffer(peerConnection, roomRef, peerId);
+    await receiveOffer(peerConnection, roomRef, peerId, nameId);
 
     const answer = await createAnswer(peerConnection);
 
-    await sendAnswer(answer, roomRef, peerId);
+    await sendAnswer(answer, roomRef, peerId, nameId);
 
-    await receiveICECandidates(peerConnection, roomRef, peerId);
+    await receiveICECandidates(peerConnection, roomRef, peerId, nameId);
 
     document.querySelector('#hangupBtn').addEventListener('click', () => peerConnection.close());
 
     closeConnection(peerConnection, roomRef, peerId);
 
     restartConnection(peerConnection, roomRef, peerId);
+
+    document.querySelector('#screenShareButton').addEventListener('click', async () => await contentToggleButton(roomRef, peerConnection));
+    document.getElementById('localVideo').srcObject.getVideoTracks()[0].onended = async function () {
+        await contentToggleButton(roomRef, peerConnection);
+    };
+
+    document.querySelector('#screenShareButton').classList.remove("hidden");
 }
 
 function restartConnection(peerConnection, roomRef, peerId) {
@@ -308,14 +429,14 @@ async function createRoom() {
         )
     };
 
-    await addUserToRoom(roomRef);
+    nameId = await addUserToRoom(roomRef);
 
     roomRef.onSnapshot(async snapshot => {
         if (snapshot.exists) {
             const peers = snapshot.data().names
-            if (peers.length != 1) {
+            if (peers.length != 1 && peers[peers.length - 1] != contentId) {
                 console.log('Sending request to: ' + peers[peers.length - 1]);
-                await peerRequestConnection(peers[peers.length - 1], roomRef);
+                await peerRequestConnection(peers[peers.length - 1], roomRef, nameId);
             }
         }
     });
@@ -353,7 +474,7 @@ async function joinRoomById(roomId) {
         document.querySelector('#joinBtn').classList.add("hidden");
         document.querySelector('#muteButton').classList.remove("hidden");
 
-        await addUserToRoom(roomRef);
+        nameId = await addUserToRoom(roomRef);
 
         console.log('Join room: ', roomId);
 
@@ -361,7 +482,7 @@ async function joinRoomById(roomId) {
             await snapshot.docChanges().forEach(async change => {
                 if (change.type === 'added') {
                     console.log("Accepting Request from: " + change.doc.id);
-                    await peerAcceptConnection(change.doc.id, roomRef);
+                    await peerAcceptConnection(change.doc.id, roomRef, nameId);
                 } else {
                     console.log("Mesh has been setup.");
                 }
@@ -371,9 +492,9 @@ async function joinRoomById(roomId) {
         roomRef.onSnapshot(async snapshot => {
             const peers = snapshot.data().names
             console.log("Current peers: " + peers);
-            if (peers[peers.length - 1] != nameId) {
+            if (peers[peers.length - 1] != nameId && peers[peers.length - 1] != contentId) {
                 console.log('Sending request to: ' + peers[peers.length - 1]);
-                await peerRequestConnection(peers[peers.length - 1], roomRef);
+                await peerRequestConnection(peers[peers.length - 1], roomRef, nameId);
             }
         });
 
@@ -385,9 +506,11 @@ async function joinRoomById(roomId) {
 }
 
 async function openUserMedia() {
-    const stream = await navigator.mediaDevices.getUserMedia(
+    cameraStream = await navigator.mediaDevices.getUserMedia(
         {video: true, audio: true});
-    document.querySelector('#localVideo').srcObject = stream;
+    document.querySelector('#localVideo').srcObject = cameraStream;
+
+    localStream = cameraStream;
 
     console.log('Stream:', document.querySelector('#localVideo').srcObject);
     document.querySelector('#joinBtn').classList.remove("hidden");
