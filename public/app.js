d153b6a17e209f4766d9b10d9f8a2ec874de84bb
Mohammad Yousuf Minhaj Zia <e0381439@u.nus.edu>
2020-06-21 13:45:35 +0800

    refactored code

--- public/app.js
+++ public/app.js
@@ -12,7 +12,6 @@ const configuration = {
 
 let roomDialog = null;
 let nameId = null;
-let contentId = null;
 let muteState = false;
 let videoState = true;
 var contentState = false;
@@ -21,6 +20,12 @@ let screenState = false;
 let cameraStream = null;
 let captureStream = null;
 let localStream = null;
+
+function isHandheld() {
+  let check = false;
+  (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+  return check;
+};
 
 function videoToggleEnable() {
     document.getElementById('videoButton').addEventListener('click', () => {
@@ -39,75 +44,6 @@ function videoToggleEnable() {
 }
 
 
-async function signalContentShare(roomRef) {
-    await roomRef.get().then(async snapshot => {
-        contentId = "peer" + (snapshot.data().names.length + 1);
-        await roomRef.update({
-            names: firebase.firestore.FieldValue.arrayUnion(contentId)
-        });
-    });
-
-    console.log("Content id is" + contentId);
-
-    roomRef.collection(contentId).doc('SDP').collection('offer').onSnapshot(async snapshot => {
-        await snapshot.docChanges().forEach(async change => {
-            if (change.type === 'added') {
-                peerId = change.doc.id;
-                const peerConnection = new RTCPeerConnection(configuration);
-                sendStream(peerConnection, captureStream);
-                signalICECandidates(peerConnection, roomRef, peerId, contentId)
-                await receiveOffer(peerConnection, roomRef, peerId, contentId);
-                const answer = await createAnswer(peerConnection);
-                await sendAnswer(answer, roomRef, peerId, contentId);
-                await receiveICECandidates(peerConnection, roomRef, peerId, contentId);
-                document.querySelector('#hangupBtn').addEventListener('click', () => peerConnection.close());
-                restartConnection(peerConnection, roomRef, peerId);
-            } else {
-                console.log("Content Sharing has been setup");
-            }
-        })
-    });
-
-    roomRef.onSnapshot(async snapshot => {
-        const peers = snapshot.data().names
-        console.log("Current peers: " + peers);
-        if (peers[peers.length - 1] != contentId) {
-            const peerConnection = new RTCPeerConnection(configuration);
-            sendStream(peerConnection, captureStream)
-            signalICECandidates(peerConnection, roomRef, peerId, nameId);
-            const offer = await createOffer(peerConnection);
-            await sendOffer(offer, roomRef, peerId, nameId);
-            await receiveAnswer(peerConnection, roomRef, peerId, nameId); 
-            await receiveICECandidates(peerConnection, roomRef, peerId, nameId);
-            document.querySelector('#hangupBtn').addEventListener('click', () => peerConnection.close());
-            restartConnection(peerConnection, roomRef, peerId);
-        }
-    });
-
-    roomRef.collection('screenShare').add({share: [nameId, contentId]});
-}
-
-function enableReceiveContentSignal(roomRef) {
-    roomRef.collection('screenShare').onSnapshot(
-        snapshot => {
-            snapshot.docChanges().forEach(async change => {
-                if (change.type === 'added') {
-                    console.log("Content sharing has been chaned ");
-                    console.log(change.doc.data());
-                    if (change.doc.data().share) {
-                        if (change.doc.data().share[0] != nameId) {
-                            document.getElementById('screenShareButton').classList.add('hidden');
-                        }
-                    } else if (change.doc.data().stopShare) {
-                        document.getElementById('screenShareButton').classList.remove('hidden');
-                    }
-                }
-            });
-
-        }
-    );
-}
-
 async function contentToggleButton(roomRef, peerConnection) {
     if (!screenState) {
         screenState = true;
@@ -125,21 +61,13 @@ async function contentToggleButton(roomRef, peerConnection) {
         captureStream.getVideoTracks()[0].onended = async function () {
             await contentToggleButton(roomRef, peerConnection);
         };
-        //await signalContentShare(roomRef);
     } else {
         stopCapture(captureStream); 
         localStream = cameraStream;
         switchStream(peerConnection, cameraStream);
-        //await roomRef.collection('disconnected').doc().set({
-            //disconnected: contentId
-        //});
-        //await roomRef.collection('screenShare').add({
-            //stopShare: contentId
-        //});
         screenState = false;
         document.getElementById('screenShareButton').innerText = 'screen_share';
         document.getElementById('screenShareButton').classList.remove('toggle');
-        contentId = null;
     }
 }
 
@@ -379,7 +307,10 @@ async function peerRequestConnection(peerId, roomRef, nameId) {
     document.getElementById('localVideo').srcObject.getVideoTracks()[0].onended = async () => {
         await contentToggleButton(roomRef, peerConnection);
     };
-    document.querySelector('#screenShareButton').classList.remove("hidden");
+
+    if (!isHandheld()) {
+        document.querySelector('#screenShareButton').classList.remove("hidden");
+    }
 }
 
 async function peerAcceptConnection(peerId, roomRef, nameId) {
@@ -411,7 +342,9 @@ async function peerAcceptConnection(peerId, roomRef, nameId) {
         await contentToggleButton(roomRef, peerConnection);
     };
 
-    document.querySelector('#screenShareButton').classList.remove("hidden");
+    if (!isHandheld()) {
+        document.querySelector('#screenShareButton').classList.remove("hidden");
+    }
 }
 
 function restartConnection(peerConnection, roomRef, peerId) {
@@ -452,6 +385,18 @@ function registerPeerConnectionListeners(peerConnection) {
     });
 }
 
+function requestConnectionToJoiningPeers(roomRef) {
+    roomRef.onSnapshot(async snapshot => {
+        if (snapshot.exists) {
+            const peers = snapshot.data().names
+            if (peers.length != 1 && peers[peers.length - 1] != nameId) {
+                console.log('Sending request to: ' + peers[peers.length - 1]);
+                await peerRequestConnection(peers[peers.length - 1], roomRef, nameId);
+            }
+        }
+    });
+}
+
 async function createRoom() {
     document.querySelector('#hangupBtn').classList.remove("hidden");
     document.querySelector('#createBtn').classList.add("hidden");
@@ -470,19 +415,10 @@ async function createRoom() {
 
     nameId = await addUserToRoom(roomRef);
 
-    roomRef.onSnapshot(async snapshot => {
-        if (snapshot.exists) {
-            const peers = snapshot.data().names
-            if (peers.length != 1 && peers[peers.length - 1] != contentId) {
-                console.log('Sending request to: ' + peers[peers.length - 1]);
-                await peerRequestConnection(peers[peers.length - 1], roomRef, nameId);
-            }
-        }
-    });
+    requestConnectionToJoiningPeers(roomRef);
 
     signalHangup(roomRef);
     console.log(`Room ID: ${roomRef.id}`);
-    enableReceiveContentSignal(roomRef);
 }
 
 function joinRoom() {
@@ -492,6 +428,19 @@ function joinRoom() {
             await joinRoomById(roomId);
         }, {once: true});
     roomDialog.open();
+}
+
+function acceptConnectionsFromCurrentPeersInParty(roomRef) {
+    roomRef.collection(nameId).doc('SDP').collection('offer').onSnapshot(async snapshot => {
+        await snapshot.docChanges().forEach(async change => {
+            if (change.type === 'added') {
+                console.log("Accepting Request from: " + change.doc.id);
+                await peerAcceptConnection(change.doc.id, roomRef, nameId);
+            } else {
+                console.log("Mesh has been setup.");
+            }
+        })
+    });
 }
 
 async function joinRoomById(roomId) {
@@ -517,31 +466,13 @@ async function joinRoomById(roomId) {
         nameId = await addUserToRoom(roomRef);
 
         console.log('Join room: ', roomId);
+        
+        acceptConnectionsFromCurrentPeersInParty(roomRef);
 
-        roomRef.collection(nameId).doc('SDP').collection('offer').onSnapshot(async snapshot => {
-            await snapshot.docChanges().forEach(async change => {
-                if (change.type === 'added') {
-                    console.log("Accepting Request from: " + change.doc.id);
-                    await peerAcceptConnection(change.doc.id, roomRef, nameId);
-                } else {
-                    console.log("Mesh has been setup.");
-                }
-            })
-        });
-
-        roomRef.onSnapshot(async snapshot => {
-            const peers = snapshot.data().names
-            console.log("Current peers: " + peers);
-            if (peers[peers.length - 1] != nameId && peers[peers.length - 1] != contentId) {
-                console.log(contentId);
-                console.log('Sending request to: ' + peers[peers.length - 1]);
-                await peerRequestConnection(peers[peers.length - 1], roomRef, nameId);
-            }
-        });
-
-        enableReceiveContentSignal(roomRef);
+        requestConnectionToJoiningPeers(roomRef);
 
         signalHangup(roomRef);
+
     } else {
         document.querySelector(
             '#currentRoom').innerText = `Room: ${roomId} - Doesn't exist!`;
@@ -566,11 +497,6 @@ function hangUp() {
         track.stop();
     });
 
-    //document.querySelector('#joinBtn').classList.add("hidden");
-    //document.querySelector('#createBtn').classList.add("hidden");
-    //document.querySelector('#hangupBtn').classList.add("hidden");
-    //document.querySelector('#currentRoom').innerText = '';
-
     window.location = window.location.pathname;
 }
 
@@ -581,22 +507,21 @@ function hideNavBarOnTap() {
 }
 
 function init() {
-
-    openUserMedia()
-
     params = new URLSearchParams(location.search);
     roomDialog = new mdc.dialog.MDCDialog(document.querySelector('#room-dialog'));
 
+    openUserMedia()
     if (params.get('roomId')) {
         console.log('Done');
         document.querySelector('#room-id').value = params.get('roomId');
         joinRoom();
     }
-
     document.querySelector('#hangupBtn').addEventListener('click', hangUp);
     document.querySelector('#createBtn').addEventListener('click', createRoom);
     document.querySelector('#joinBtn').addEventListener('click', joinRoom);
     hideNavBarOnTap();
+    muteToggleEnable();
+    videoToggleEnable();
 
     var iOS = ['iPad', 'iPhone', 'iPod'].indexOf(navigator.platform) >= 0;
     var eventName = iOS ? 'pagehide' : 'beforeunload';
@@ -604,8 +529,6 @@ function init() {
     window.addEventListener(eventName, function() {
         document.getElementById('hangupBtn').click();
     });
-    muteToggleEnable();
-    videoToggleEnable();
 }
 
 init();
